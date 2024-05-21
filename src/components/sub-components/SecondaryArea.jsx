94f3078339f12332a399c4821ee12673d377e4bc
Abubakar <abubakarsiddikl.info@gmail.com>
2024-05-22 00:07:21 +0600

    secondary area

--- src/components/sub-components/SecondaryArea.jsx
+++ src/components/sub-components/SecondaryArea.jsx
@@ -1,5 +1,6 @@
-import { Progress, Radio } from "antd";
+import { ConfigProvider, Progress, Radio } from "antd";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const options = [
   {
@@ -13,6 +14,7 @@ const options = [
 ];
 
 const SecondaryArea = ({ renderSlots, ground }) => {
+  const { slotBg, slotBorder } = useSelector((state) => state.customizeSec);
   const [value3, setValue3] = useState("glove");
   const onChange3 = ({ target: { value } }) => {
     console.log("radio3 checked", value);
@@ -21,18 +23,42 @@ const SecondaryArea = ({ renderSlots, ground }) => {
   return (
     <div className="secondaryArea ">
       <div className="secondaryAreaTop">
-        <Progress percent={50} showInfo={false} size={["100%", 30]} />
-        <Radio.Group
-          className="w-full my-2"
-          size="large"
-          options={options}
-          onChange={onChange3}
-          value={value3}
-          optionType="button"
-          buttonStyle="solid"
+        <Progress
+          percent={50}
+          showInfo={false}
+          size={["100%", 35]}
+          strokeColor={slotBorder}
+          trailColor="#555"
         />
+        <ConfigProvider
+          theme={{
+            components: {
+              Radio: {
+                buttonBg: slotBg,
+                buttonColor: "white",
+                buttonSolidCheckedColor: "white",
+                buttonSolidCheckedBg: slotBorder,
+                buttonSolidCheckedActiveBg: slotBorder,
+                buttonSolidCheckedHoverBg: slotBorder,
+              },
+            },
+          }}
+        >
+          <Radio.Group
+            className="w-full my-2 mt-4"
+            size="large"
+            options={options}
+            onChange={onChange3}
+            value={value3}
+            optionType="button"
+            buttonStyle="solid"
+            style={{ border: "none" }}
+          />
+        </ConfigProvider>
       </div>
-      <div className="section">{renderSlots(ground)}</div>
+      <div className="border py-2 rounded-[20px] px-3" style={{ borderColor: slotBorder }}>
+        <div className="section">{renderSlots(ground)}</div>
+      </div>
     </div>
   );
 };
