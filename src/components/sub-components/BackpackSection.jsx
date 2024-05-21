9e4d45701b3bbe597fa3ef0b4ebf3147d10ca043
Abubakar <abubakarsiddikl.info@gmail.com>
2024-05-21 14:23:53 +0600

    backpack

--- src/components/sub-components/BackpackSection.jsx
+++ src/components/sub-components/BackpackSection.jsx
@@ -4,15 +4,33 @@ const BackpackSection = ({ renderSlots, backpack }) => {
       <div className="backpackSectionTop mb-2.5">
         <div className="me-1">
           <div className="activeBackpack"></div>
-          <div className="flex flex-col	">
-            <h3 className="text-lg  text-zinc-300 ms-2 mb-0">Large Backpack</h3>
-            <p className="mb-0">
-              Capacity <span>12</span>/ <span>50</span> kg
+          <div className="w-full ms-2">
+            <div className="flex flex-col	border-b mb-2 pb-2">
+              <h3 className="text-lg  text-zinc-300  mb-0">Large Backpack</h3>
+              <p className="mb-0   text-zinc-300">
+                Capacity <span>12</span>/ <span>50</span> kg
+              </p>
+            </div>
+            <p className=" text-[11px]">
+              This is a backpack item. <br />
+              You can use it carry more.
             </p>
           </div>
         </div>
-        <div className="ms-1">
-          <h3>Small Backpack</h3>
+        <div className="">
+          <div className="activeBackpack"></div>
+          <div className="w-full ms-2">
+            <div className="flex flex-col	border-b mb-2 pb-2">
+              <h3 className="text-lg  text-zinc-300  mb-0">Small Backpack</h3>
+              <p className="mb-0   text-zinc-300">
+                Capacity <span>12</span>/ <span>50</span> kg
+              </p>
+            </div>
+            <p className=" text-[11px]">
+              This is a backpack item. <br />
+              You can use it carry more.
+            </p>
+          </div>
         </div>
       </div>
       <div className="section">{renderSlots(backpack)}</div>
