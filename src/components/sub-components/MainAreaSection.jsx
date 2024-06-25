63772c78ab253520d3a2c40a5bd131daa9035378
Abubakar <abubakarsiddikl.info@gmail.com>
2024-06-25 12:17:42 +0600

    space

--- src/components/sub-components/MainAreaSection.jsx
+++ src/components/sub-components/MainAreaSection.jsx
@@ -64,14 +64,22 @@ const MainAreaSection = ({ inventory, isModalOpen, setIsModalOpen }) => {
         className="mainAreaTop pt-1 border-b pb-1"
         style={{ borderBottom: `4px solid ${slotBg}`, color: textColor }}
       >
-        <div className="mb-3 pt-2">
-          <div className="flex items-center	w-[50%]">
-            <div
-              className="border rounded-full p-2 text-xl me-2"
-              style={{ backgroundColor: hudBg, borderColor: hudBorderColor }}
-            >
-              <BsBoxes />
-            </div>
+        <div className="flex items-center mb-3 pt-2">
+          <div
+            className="border rounded-full p-2 text-xl me-2"
+            style={{ backgroundColor: hudBg, borderColor: hudBorderColor }}
+          >
+            <BsBoxes />
+          </div>
+          <div
+            className={`${
+              inventory?.label?.length < 12
+                ? "w-[20%]"
+                : inventory?.label?.length < 25
+                ? "w-[30%]"
+                : "w-[45%]"
+            } me-2  text-center`}
+          >
             <p
               className="border px-5 py-1  text-lg rounded-[20px]"
               style={{ backgroundColor: hudBg, borderColor: hudBorderColor }}
@@ -79,14 +87,16 @@ const MainAreaSection = ({ inventory, isModalOpen, setIsModalOpen }) => {
               {inventory?.label}
             </p>
           </div>
-          <Progress
-            percent={Number(inventory?.weightPercent)}
-            showInfo={false}
-            size={["100%", 35]}
-            strokeColor={calculateRGBRev(Number(inventory?.weightPercent))}
-            trailColor={hudBg}
-            style={{ border: `2px solid ${hudBorderColor}`, borderRadius: 50 }}
-          />
+          <div className="w-[70%] me-2">
+            <Progress
+              percent={Number(inventory?.weightPercent)}
+              showInfo={false}
+              size={["100%", 35]}
+              strokeColor={calculateRGBRev(Number(inventory?.weightPercent))}
+              trailColor={hudBg}
+              style={{ border: `2px solid ${hudBorderColor}`, borderRadius: 50 }}
+            />
+          </div>
           <div
             className="border rounded-full  p-2 text-xl cursor-pointer"
             style={{ backgroundColor: hudBg, borderColor: hudBorderColor }}
