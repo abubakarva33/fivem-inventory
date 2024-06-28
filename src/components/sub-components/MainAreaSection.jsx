29ed3cf31fcb5f279426837b79eb86fcd35d7b6d
MenanAk47 <amanullahmenan@gmail.com>
2024-06-29 04:49:08 +0600

    hud icon

--- src/components/sub-components/MainAreaSection.jsx
+++ src/components/sub-components/MainAreaSection.jsx
@@ -78,12 +78,25 @@ const MainAreaSection = ({ inventory, isModalOpen, setIsModalOpen, openBackpacks
             className="mainAreaTop pt-1 border-b pb-[6px]"
             style={{ borderBottom: `4px solid ${slotBg}`, color: textColor }}
           >
-            <div className="flex items-center mb-3 pt-2">
+            <div className="flex items-center mb-3 pt-2 text-xl">
               <div
-                className="border rounded-full p-2 text-xl me-2"
+                className="flex items-center border me-2 px-4 py-1 rounded-[20px] "
                 style={{ backgroundColor: hudBg, borderColor: hudBorderColor }}
               >
-                <BsBoxes />
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    style={{ height: 20, marginRight: 2}}
+                  >
+                  <path
+                    fill={textColor}
+                    d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64zm96 320h64c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM144 64h96c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z"
+                  />
+                </svg>
+                <span
+                  style={{ fontSize: 18}}
+                  >{hudData.id || 20}
+                </span>
               </div>
               <div
                 className={`${
@@ -115,7 +128,16 @@ const MainAreaSection = ({ inventory, isModalOpen, setIsModalOpen, openBackpacks
                 className="border rounded-full  p-2 text-xl cursor-pointer"
                 style={{ backgroundColor: hudBg, borderColor: hudBorderColor }}
               >
-                <BsBoxes onClick={() => setIsModalOpen(!isModalOpen)} />
+                <svg onClick={() => setIsModalOpen(!isModalOpen)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    style={{ height: 20}}
+                  >
+                  <path
+                    fill={textColor}
+                    d="M339.3 367.1c27.3-3.9 51.9-19.4 67.2-42.9L568.2 74.1c12.6-19.5 9.4-45.3-7.6-61.2S517.7-4.4 499.1 9.6L262.4 187.2c-24 18-38.2 46.1-38.4 76.1L339.3 367.1zm-19.6 25.4l-116-104.4C143.9 290.3 96 339.6 96 400c0 3.9 .2 7.8 .6 11.6C98.4 429.1 86.4 448 68.8 448H64c-17.7 0-32 14.3-32 32s14.3 32 32 32H208c61.9 0 112-50.1 112-112c0-2.5-.1-5-.2-7.5z"
+                  />
+                </svg>
               </div>
             </div>
             <div className="flex items-center text-xl justify-between mb-2">
